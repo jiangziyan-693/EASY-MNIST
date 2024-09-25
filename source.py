f85e75002a950cb53c35ab129d95a3a0552d5456
jiangziyan-693 <150317638+jiangziyan-693@users.noreply.github.com>
2024-09-25 14:48:23 +0800

    Add files via upload

--- source.py
+++ source.py
@@ -17,26 +17,18 @@ class Net(torch.nn.Module):
                 activation = torch.nn.Tanh()
                 
             self.model = torch.nn.Sequential(
-            #The size of the picture is 28x28
-            torch.nn.Conv2d(in_channels = 1,out_channels = 16,kernel_size = 3,stride = 1,padding = 1),
+            torch.nn.Flatten(),  # 将输入的 28x28 图像展平成 784 维向量
+            torch.nn.Linear(784, 512),
             activation,
-            torch.nn.MaxPool2d(kernel_size = 2,stride = 2),
-            
-            #The size of the picture is 14x14
-            torch.nn.Conv2d(in_channels = 16,out_channels = 32,kernel_size = 3,stride = 1,padding = 1),
+            torch.nn.Linear(512, 256),
             activation,
-            torch.nn.MaxPool2d(kernel_size = 2,stride = 2),
-            
-            #The size of the picture is 7x7
-            torch.nn.Conv2d(in_channels = 32,out_channels = 64,kernel_size = 3,stride = 1,padding = 1),
+            torch.nn.Linear(256, 128),
             activation,
-            
-            torch.nn.Flatten(),
-            torch.nn.Linear(in_features = 7 * 7 * 64,out_features = 128),
+            torch.nn.Linear(128, 64),
             activation,
-            torch.nn.Linear(in_features = 128,out_features = 10),
+            torch.nn.Linear(64, 10),
             torch.nn.Softmax(dim=1)
-            )
+        )
         
         def forward(self,input):
             output = self.model(input)
@@ -53,7 +45,7 @@ def __main__():
     BATCH_SIZE = config['BATCH_SIZE']
     EPOCHS = config['EPOCHS']
     LEARNING_RATE = config['LEARNING_RATE']
-    activation_function = 'ReLU'
+    activation_function = config['ACTIVATION_FUNCTION']
     trainData = torchvision.datasets.MNIST('./data/',train = True,transform = transform,download = True)
     testData = torchvision.datasets.MNIST('./data/',train = False,transform = transform)
 
